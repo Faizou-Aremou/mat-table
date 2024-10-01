2c724d481af6b84d91c2c90cd208399194b76962
Faïzou Aremou <faizouaremou@gmail.com>
2024-10-01 19:59:53 +0200

    table modeling progress

--- src/table.ts
+++ src/table.ts
@@ -5,6 +5,7 @@ import {
   CdkNoDataRow,
   CdkRowDef,
   CdkTableDataSourceInput,
+  RowOutlet,
 } from '@angular/cdk/table';
 import { QueryList, TrackByFunction } from '@angular/core';
 
@@ -30,6 +31,7 @@ type TableInitialized = 'tableInit';
 type TableContentInitialized = 'tableContentInit';
 type TableContentChecked = 'tableContentChecked';
 type TableDestroyed = 'tableDestroyed';
+
 type ColumnDefinitionIsAdded = {
   columnDef: CdkColumnDef;
 };
@@ -42,14 +44,26 @@ type RowDefinitionIsAdded = {
 type RowDefinitionIsRemoved = {
   columnDef: CdkColumnDef;
 };
+type HeaderRowDefinitionIsAdded = 'headerRowDefinitionIsAdded';
+type HeaderRowDefinitionIsRemoved = 'headerRowDefinitionIsRemoved';
+type FooterRowDefinitionIsAdded = 'footerRowDefinitionIsAdded';
+type FooterRowDefinitionIsRemoved = 'footerRowDefinitionIsRemoved';
+type NoDataRowIsSet = 'noDataRowIsSet';
+type StickyHeaderRowStylesIsUpdated = 'StickyHeaderRowStylesIsUpdated';
+type ContentRendered = 'ContentRendered';
+
+type StickyFooterRowStylesIsUpdated = 'StickyFooterRowStylesIsUpdated';
+type StickyColumnStylesIsUpdated = 'StickyColumnStylesIsUpdated'
+
 
 // WorkFlows actions
-type Init = () => Initialized;
-type OnInit = () => TableInitialized;
-type AfterContentInit = () => TableContentInitialized;
-type AfterContentChecked = () => TableContentChecked;
-type OnDestroy = () => TableDestroyed;
-type RenderRows = () => ContentChanged;
+type Init<T> = (table: Table<T>) => Initialized;
+type OnInit<T> = (table: Table<T>) => TableInitialized;
+type AfterContentInit<T> = (table: Table<T>) => TableContentInitialized;
+type AfterContentChecked<T> = (table: Table<T>) => TableContentChecked;
+type OnDestroy<T> = (table: Table<T>) => TableDestroyed;
+type RenderRows<T> = (table: Table<T>) => ContentChanged;
+type Render<T> = (table: Table<T>) => ContentRendered;
 type AddColumnDef<T> = (
   table: Table<T>,
   columnDef: CdkColumnDef
@@ -62,7 +76,41 @@ type AddRowDef<T> = (
   table: Table<T>,
   rowDef: CdkColumnDef
 ) => RowDefinitionIsAdded;
-type removeRowDef<T> = (
+type RemoveRowDef<T> = (
   table: Table<T>,
-  rowDef: CdkColumnDef
+  rowDef: CdkRowDef<T>
 ) => RowDefinitionIsRemoved;
+type AddHeaderRowDef<T> = (
+  table: Table<T>,
+  headerRowDef: CdkHeaderRowDef
+) => HeaderRowDefinitionIsAdded;
+type RemoveHeaderRowDef<T> = (
+  table: Table<T>,
+  headerRowDef: CdkHeaderRowDef
+) => HeaderRowDefinitionIsRemoved;
+type AddFooterRowDef<T> = (
+  table: Table<T>,
+  footerRowDef: CdkFooterRowDef
+) => FooterRowDefinitionIsAdded;
+type RemoveFooterRowDef<T> = (
+  table: Table<T>,
+  footerRowDef: CdkFooterRowDef
+) => FooterRowDefinitionIsRemoved;
+
+type SetNoDataRow<T> = (
+  table: Table<T>,
+  noDataRow: CdkNoDataRow | null
+) => NoDataRowIsSet;
+
+type UpdateStickyHeaderRowStyles<T> = (
+  table: Table<T>
+) => StickyHeaderRowStylesIsUpdated;
+type UpdateStickyFooterRowStyles<T> = (
+  table: Table<T>
+) => StickyFooterRowStylesIsUpdated;
+type UpdateStickyColumnStyles<T> = (
+  table: Table<T>
+) => StickyColumnStylesIsUpdated;
+
+type GetRenderedRows<T> = (rowOutlet: RowOutlet) => HTMLElement[];
+type GetRowDefs<T> = () => CdkRowDef<T>[];
